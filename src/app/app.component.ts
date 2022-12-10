f2504131c3bf4b52bf75cff9ead0583ef8d062da
fcolic <fcolic8920rn@raf.rs>
2022-12-10 18:34:47 +0100

    odjavljivanje i sakrivanje putanja iz nav-bara

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RequestService } from './request.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'domaci3_front';
+  constructor(public requestService: RequestService, private router: Router){}
+  ngOnInit(): void{
+    console.log("hello from app");
+    console.log("iz app: can_read - " + this.requestService.userInfo.can_read);
+  }
+  logout(): void{
+    this.requestService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import jwtDecode from 'jwt-decode';
+import { UserInfo } from 'src/model';
 import { RequestService } from '../request.service';
 
 @Component({
@@ -12,7 +14,7 @@ export class LoginComponent implements OnInit {
   
   loginGroup: FormGroup;
 
-  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private requestService: RequestService) {
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private requestService: RequestService, private router: Router) {
     this.loginGroup = this.formBuilder.group({
       lgEmail: ['', Validators.required],
       lgPassword: ['', Validators.required]
@@ -27,6 +29,15 @@ export class LoginComponent implements OnInit {
       if(res.jwt.length == 0)
       localStorage.removeItem("jwt")
     localStorage.setItem("jwt", res.jwt);
+    this.requestService.userInfo = jwtDecode(res.jwt) as UserInfo;
+    if(this.requestService.userInfo.can_read)
+      this.router.navigate(['/read']);
+    else if(this.requestService.userInfo.can_create)
+      this.router.navigate(['create']);
+    else if(!this.requestService.userInfo.can_delete && !this.requestService.userInfo.can_update){
+      alert("Korisnik nema nijednu permisiju! Odjavljivanje!");
+      this.requestService.logout();
+    }
     })
   }
 
